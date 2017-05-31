a842248c9a9af744b53fb93157bbe1b1222a0f0b
TonoFilth <tono_is_coming@hotmail.com>
2017-05-31 23:35:52 +0200

    Modified sublime build script

--- scripts/linux/build_sublime.sh
+++ scripts/linux/build_sublime.sh
@@ -9,8 +9,8 @@ function get_engine_module
 {
     local res=""
 
-    [[ $1 =~ ^.*bitwise/src/engine/public/bitwise/([a-z_]+).* ]]  && res=${BASH_REMATCH[1]}
-    [[ $1 =~ ^.*bitwise/src/engine/private/bitwise/([a-z_]+).* ]] && res=${BASH_REMATCH[1]}
+    [[ $1 =~ ^.*/src/engine/public/bitwise/([a-z_]+).* ]]  && res=${BASH_REMATCH[1]}
+    [[ $1 =~ ^.*/src/engine/private/bitwise/([a-z_]+).* ]] && res=${BASH_REMATCH[1]}
 
     echo $res
 }
@@ -19,7 +19,7 @@ function get_sample_engine_program
 {
     local res=""
 
-    [[ $1 =~ ^.*bitwise/samples/engine/([a-z_]+/[a-z_]+)/.* ]] && res=${BASH_REMATCH[1]}
+    [[ $1 =~ ^.*/samples/engine/([a-z_]+/[a-z_]+)/.* ]] && res=${BASH_REMATCH[1]}
 
     echo $res
 }
@@ -27,8 +27,8 @@ function get_sample_engine_program
 program=""
 program_path=bin/samples/engine/
 
-if   [[ $1 == *"bitwise/src/engine/"*     ]]; then program="./bin/samples/engine/"$(get_engine_module $1)"/quick_test"
-elif [[ $1 == *"bitwise/samples/engine/"* ]]; then program="./bin/samples/engine/"$(get_sample_engine_program $1)
+if   [[ $1 == *"/src/engine/"*     ]]; then program="./bin/samples/engine/"$(get_engine_module $1)"/quick_test"
+elif [[ $1 == *"/samples/engine/"* ]]; then program="./bin/samples/engine/"$(get_sample_engine_program $1)
 fi
 
 if [ $program -a -e $program ]; then
